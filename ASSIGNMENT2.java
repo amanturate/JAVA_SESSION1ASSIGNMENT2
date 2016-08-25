927ac55dc31478379ee436d808f014aa9e165f0e
Aman Turate <amanturate@users.noreply.github.com>
2016-08-25 14:41:45 +0530

    Add files via upload

---
+++ ASSIGNMENT2.java
@@ -0,0 +1,16 @@
+
+public class ASSIGNMENT2 {
+
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 10;
+		
+		int c = a-- - --a;
+		int d = a--;
+		int e = a>>2;
+		int f = a&b;
+		
+		System.out.println(c + "\n" + d + "\n" + e + "\n" + f);
+	}
+	
+}
